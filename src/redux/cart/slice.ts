56a55ff9b0eefdcaee4aa10e39d6217c0c7bd3bb
EXTRAS12 <extra-kent@mail.ru>
2022-07-10 14:32:31 +0300

    refactoring

--- src/redux/cart/slice.ts
+++ src/redux/cart/slice.ts
@@ -1,17 +1,17 @@
-import { CartSliceState, CartItem } from './types';
-import { calcTotalPrice } from './../../utils/calcTotalPrice';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { calcTotalPrice } from '../../utils/calcTotalPrice';
 import { getCartFromLS } from '../../utils/getCartFromLS';
+import { CartItem, CartSliceState } from './types';
 
+const initialState: CartSliceState = getCartFromLS();
 
-const initialState: CartSliceState = getCartFromLS()
-
-export const cartSlice = createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addItem(state, action: PayloadAction<CartItem>) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
       if (findItem) {
         findItem.count++;
       } else {
@@ -23,15 +23,18 @@ export const cartSlice = createSlice({
 
       state.totalPrice = calcTotalPrice(state.items);
     },
-
     minusItem(state, action: PayloadAction<string>) {
       const findItem = state.items.find((obj) => obj.id === action.payload);
+
       if (findItem) {
         findItem.count--;
       }
+
+      state.totalPrice = calcTotalPrice(state.items);
     },
     removeItem(state, action: PayloadAction<string>) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = calcTotalPrice(state.items);
     },
     clearItems(state) {
       state.items = [];
@@ -40,7 +43,6 @@ export const cartSlice = createSlice({
   },
 });
 
-
-export const { addItem, removeItem, clearItems, minusItem } = cartSlice.actions;
+export const { addItem, removeItem, minusItem, clearItems } = cartSlice.actions;
 
 export default cartSlice.reducer;
